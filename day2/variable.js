c46e18ef3bdb159efaedcee007cc1a9c3b8156ce
Siddheshwar Gupta <siddheshwargupta081@gmail.com>
2025-05-14 13:08:24 +0545

    All JS

---
+++ day2/variable.js
@@ -0,0 +1,98 @@
+// /* 
+// // there are 3 methods to initialize variable:
+// //  #variable is a name given to a space in  a memory for memory allocation.
+
+
+//  // 1) var
+
+
+// function var1(){
+// if (1){
+// var a=10;
+// }
+// console.log(a,"var");
+// }
+// Var1();
+
+
+// // 2) let
+
+
+// function let1(){
+// if (1){
+// let a=10;
+// }
+// console.log(a,"let");
+// }
+// let1(); //ReferenceError: a is not defined
+
+
+//  // 3) const
+
+// const fullName ="Nepathya";
+// console.log(fullName);
+
+// */
+// // const fullName ="This is an example of constant";
+// // console.log(fullName);
+// /*
+// function var1(){
+//     if (1){
+//         var num=55;
+//     }
+//     console.log(num);
+// }
+// var1();
+
+
+// function let1(){
+//     if (1){
+//         let Num=50;
+//     }
+//     console.log(Num ,"let");
+// }
+// let1();
+// //let num=prompt("Enter the number for factorial");
+// let num=5;
+// let Num ="five";
+// if(num==Num){
+//     console.log("ok");
+// }
+// else{
+//     console.log("false");
+// }*/
+
+
+// //data types in javaScript
+
+// // string: 
+//  let greet ="Hello";
+//  console.log(typeof greet);
+
+// //Number:
+// let Number=423423;
+// //Boolean:
+// let isFollow=true;
+// let Isfollow=false;
+// //Null
+// let c=null;
+// console.log(typeof c);
+// //undefined:
+// let d=undefined;    // undefined means initialize garne but value na hune for e.g  let a;   
+// console.log(typeof d);
+// //object,
+// //Array
+//  const student={
+//     title:"sid",
+//     age:23,
+//     address:"parasi",
+//     isFollow:true,
+//  };
+//  student["age"] +=1;
+//  console.log(student["age"]);
+//  console.log( student.isFollow);
+ 
+
+ 
+
+
---
+++ day3/typeConversion.js
@@ -0,0 +1,54 @@
+// // console.log("Hello World !!");
+// //  let var1 =5;
+// //  let var2 ="5";
+// //  console.log("var1 = var2",var1===var2);
+
+
+// // //explicit conversion
+// // let a ="10";// instead of 10 if we write name then it's O/P:NaN i.e not a number.
+// // let b= 20;
+// // let sum = Number (a)+ b;  // it converts the string into number and then perform arithmetic operation
+// // console.log(sum);
+
+
+// let newBtn =document.createElement("button");
+// newBtn.innerText = "Click Me !!";
+// newBtn.style.color="red";
+// newBtn.style.backgroundColor = "green";
+
+//   let newBtn =document.querySelector("body");
+//  body.prepend(newBtn);
+
+// // const num1=prompt("Enter first number");
+// // const num2 =prompt("Enter second number");
+// // // const Sum = num1+num2;  // It only concate num1 and num2 but don't perform arithmetic operation
+// // const diff=num1-num2;//if it is minus sign then it doesn't concate it but it perform arithmetic subtraction opreation
+// // console.log(diff);
+// // const Sum =Number(num1)+Number(num2);//It perform arithmetic operation becoz it explicitly converted string into number
+// // console.log(Sum);
+// const num1=prompt("Enter first number");
+//  const num2 =prompt("Enter second number");
+//  const add= parseInt(num1)+parseInt(num2);//convert string into number and perform addition
+// console.log(add);
+
+// // var num=5;
+// // var num=50;
+// // console.log(num);
+// // let a=50;
+// // a=55;
+// // console.log(a);
+
+
+// let str =prompt("Enter first String:");
+// let num =prompt("Enter any Number:");
+// console.log(str+str);//string +string  // if we take string="10" & number=5 o/p:1010
+// console.log(str+num);//string+number // o/p:105
+// console.log(num+str);//number+string  o/p:510
+// console.log(num+num);//number + number o/p:10
+// console.log(str-str);//string - string  o/p: 0
+// console.log(num-str);//number -string o/p: -5
+// console.log(str-num); //string - number o/p:5
+// console.log(num-num);//number-number  o/p:0
+
+
+
